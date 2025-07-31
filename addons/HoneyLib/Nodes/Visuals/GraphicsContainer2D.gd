28d97f7f3da5e26badd0a3171208d2489b83433f
Hive Bee <mviniciuslinux@gmail.com>
2025-07-30 23:29:47 -0300

    Removed GraphicsContainer2D

--- addons/HoneyLib/Nodes/Visuals/GraphicsContainer2D.gd
+++
@@ -1,9 +0,0 @@
-tool
-class_name GraphicsContainer2D,preload("res://addons/HoneyLib/Icons/Visuals/GraphicsContainer2D.svg")
-extends Component2D
-
-
-
-## Returns class name for reflection.
-func get_class() -> String:
-	return "GraphicsContainer2D"
