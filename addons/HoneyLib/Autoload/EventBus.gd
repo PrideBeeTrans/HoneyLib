d3238cc09352215b5b68c1159c5a75dce400988e
Hive Bee <mviniciuslinux@gmail.com>
2025-08-03 11:01:05 -0300

    Added EventBus functionality

--- addons/HoneyLib/Autoload/EventBus.gd
+++ addons/HoneyLib/Autoload/EventBus.gd
@@ -1,16 +1,43 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+var listeners := {}
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func add_listener(event_name: String,func_ref: FuncRef,priority: int = 0,once: bool = false) -> void:
+	if not listeners.has(event_name):
+		listeners[event_name] = []
+	listeners[event_name].append({
+		"func_ref": func_ref,
+		"priority": priority,
+		"once": once
+	})
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func remove_listener(event_name: String,func_ref: FuncRef) -> void:
+	if not listeners.has(event_name):
+		return
+	
+	var filtered := []
+	for entry in listeners[event_name]:
+		if entry.func_ref != func_ref:
+			filtered.append(entry)
+	
+	listeners[event_name] = filtered
+
+
+func emit(event_name: String,payload: Dictionary = {}) -> void:
+	if not listeners.has(event_name):
+		return
+	
+	var to_remove := []
+	for i in listeners[event_name].size():
+		var entry := listeners[event_name][i] as Dictionary
+		var func_ref := entry.func_ref as FuncRef
+		var once := entry.once as bool
+		func_ref.call_func()
+		if once == true:
+			to_remove.append(i)
+
+
+func _sort_priority_desc(a: Dictionary,b: Dictionary) -> Dictionary:
+	return a.priority > b.priority
